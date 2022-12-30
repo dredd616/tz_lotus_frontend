37603b71416b7c36b488b26663723a011fb9227e
dredd616 <judge616dredd@gmail.com>
2022-12-30 23:10:19 +0300

    fix api url

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-export const BASE_URL = 'https://tz-lotus-backend.onrender.com/';
+export const BASE_URL = 'https://tz-lotus-backend.onrender.com/api';
 export const instance = axios.create({ baseURL: BASE_URL });
 
 instance.interceptors.response.use(
